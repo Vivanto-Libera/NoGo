5079cc3f40e4f25a8791fdd43792d0dc26acf156
Vivanto <1425078256@qq.com>
2026-03-13 18:15:47 +0800

    Started to using godot. And add stone and board

---
+++ NoGo_GD/Stone.cs
@@ -0,0 +1,37 @@
+using Godot;
+using NoGo;
+using System;
+using static NoGo.StoneColor;
+
+public partial class Stone : Node2D
+{
+	public void PlayStone(StoneColor color) 
+	{
+		if (color == WHITE) 
+		{
+			GetNode<TextureRect>("Black").SetDeferred(TextureRect.PropertyName.Visible, false);
+			GetNode<TextureRect>("White").SetDeferred(TextureRect.PropertyName.Visible, true);
+		}
+		else 
+		{
+			GetNode<TextureRect>("Black").SetDeferred(TextureRect.PropertyName.Visible, true);
+			GetNode<TextureRect>("White").SetDeferred(TextureRect.PropertyName.Visible, false);
+		}
+		GetNode<Button>("Button").SetDeferred(Button.PropertyName.Disabled, true);
+	}
+	public void SetCrossVisible(bool visible) 
+	{
+		GetNode<TextureRect>("Cross").SetDeferred(TextureRect.PropertyName.Visible, visible);
+	}
+	public void StartGame() 
+	{
+		GetNode<Button>("Button").SetDeferred(Button.PropertyName.Disabled, false);
+	}
+	public void Reset() 
+	{
+		GetNode<TextureRect>("Black").SetDeferred(TextureRect.PropertyName.Visible, false);
+		GetNode<TextureRect>("White").SetDeferred(TextureRect.PropertyName.Visible, false);
+		GetNode<TextureRect>("Cross").SetDeferred(TextureRect.PropertyName.Visible, false);
+		GetNode<Button>("Button").SetDeferred(Button.PropertyName.Disabled, true);
+	}
+}
